e7625aaeab1ae92339f38259a55b8499ba247927
Muhammad Hasan <muhammadhasan3866@gmail.com>
2023-03-01 07:52:12 +0500

    addition

--- mathExpression.js
+++ mathExpression.js
@@ -53,6 +53,58 @@
 
 //-------------------------------- Question no 3
 
+// ----------------------------------------a
+// let num1 = "" ;
+
+// ----------------------------------------b
+
+// document.write( "Value after variable declaration is :?? <br>")
+
+// ----------------------------------------c
+
+// num1 = 10
+
+// -----------------------------------------d
+// document.write(`Initial value: ${num1}<br>`)
+
+// -------------------------------------------e
+
+//-------------------------------- increment the variable
+
+// var incrementNum = ++num1
+
+// --------------------------------------------f
+
+// document.write(`value after increment is ${incrementNum}<br> `)
+
+// ---------------------------------------------g
+
+// incrementNum=num1+7
+
+// ---------------------------------------------h
+
+// document.write(`value after addition is :${incrementNum}<br>`)
+
+//----------------------------------- Decrement the variable
+// ------------------------------------------------i
+
+// var incrementNum = --incrementNum
+
+// ------------------------------------------------j
+
+// document.write(`value after decrement is ${incrementNum}<br> `)
+
+// -------------------------------------------------k
+
+// var num2=num1%3
+
+// ---------------------------------------------------l
+
+// document.write(`the output is ${num2}`)
+
+
+
+
 
 
 
@@ -68,28 +120,50 @@
 
 //--------------------------------- question no 5
 
-var table = + prompt("which table you want to learn")
+// var table = + prompt("which table you want to learn")
 
-document.write(`Table of ${table} <br>`)
+// document.write(`Table of ${table} <br>`)
 
-document.write(`${table} X 1 = ${table *1}<br>`)
+// document.write(`${table} X 1 = ${table *1}<br>`)
 
-document.write(`${table} X 2 = ${table *2}<br>`)
+// document.write(`${table} X 2 = ${table *2}<br>`)
 
-document.write(`${table} X 3 = ${table *3}<br>`)
+// document.write(`${table} X 3 = ${table *3}<br>`)
 
-document.write(`${table} X 4 = ${table *4}<br>`)
+// document.write(`${table} X 4 = ${table *4}<br>`)
 
-document.write(`${table} X 5 = ${table *5}<br>`)
+// document.write(`${table} X 5 = ${table *5}<br>`)
 
-document.write(`${table} X 6 = ${table *6}<br>`)
+// document.write(`${table} X 6 = ${table *6}<br>`)
 
-document.write(`${table} X 7 = ${table *7}<br>`)
+// document.write(`${table} X 7 = ${table *7}<br>`)
 
-document.write(`${table} X 8 = ${table *8}<br>`)
+// document.write(`${table} X 8 = ${table *8}<br>`)
 
-document.write(`${table} X 9 = ${table *9}<br>`)
+// document.write(`${table} X 9 = ${table *9}<br>`)
 
-document.write(`${table} X 10 = ${table *10}<br>`)
+// document.write(`${table} X 10 = ${table *10}<br>`)
 
 
+//------------------------------------ Question no 6
+
+//------------------------------ temperature or converter
+
+// -----------------------------------------a
+var Celsius = prompt("What is Celsius ?")
+
+// ------------------------------------------b
+
+var fahrenheit = prompt("What is fahrenheit ?")
+
+// -------------------------------------------c
+
+var celciusTemperature = "c= (f - 32) x 5 / 9"
+
+
+var fahrenheitTemperature ="f=(c x 9 / 5) + 32"
+
+document.write(`${celciusTemperature} <br>`)
+
+
+document.write(` ${fahrenheitTemperature}  <br>`)
